87eba711be293bc5e124b8b3ca3f01bb21c89549
ram <ram@mail.com>
2020-04-21 14:11:14 +0530

    update

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,20 +3,32 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-   
+    repository_url = 'https://github.com/rameshz23/vm-jenk.git'
+    pipeline {
+        environment {
+          gitCredentialID='ram'
+        }
+
 	pipeline {
             options {
                 buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
                 }
-	       
+
 	agent any
         stages {
             stage('Clone') {
                 steps {
                     echo "this should be skipped, but it does not ("
+                    dir("$WORKSPACE/") {
+                  git(
+                    url: config.repository_url,
+                    credentialsId: env.gitCredentialID,
+                    branch: master
+                  )
+                }
                 }
             }
-            
+
            stage ('FTP') {
 		agent{
 			docker { image 'aequitas/ftp-resource' }
@@ -24,14 +36,14 @@ def call(body) {
             	steps {
 		      sh """ env > envfile
 		      cat envfile """// log.info 'Starting'
-		      script { 
+		      script {
 		               echo USER //log.info 'Starting'
 		               echo SHELL //log.warning 'Nothing to do!'
 		  	       //currentBuild.displayName = "$env.NEW_BUILDNUMBER"
 		       }
 		}
 	    }
-		
+
      }
   }
 }
